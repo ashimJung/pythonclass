50d9c58e23ab6df07383c8a153327e54247aa065
ashimJung <ashimkarki420@gmail.com>
2024-07-09 13:50:37 +0545

    started pandas

---
+++ pandas/data_operation.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+
+print(pd.__version__)
+
+dist= {
+  'head':{
+    'name':'Ashim J. Karki',
+    'address':'kathamndu',
+    'number':123456
+  },
+  'staff1':{
+    'name':'Rajesh hamal',
+    'address':'lalitpur',
+    'number':123999
+  }
+}
+#to convert dictionary to data frame we can use pd.DataFrame
+df=pd.DataFrame(dist)
+print(df)
+print(df.head())
+ 
+
+#to read datav from csv
+df_Csv = pd.read_csv('/Users/ashim/Desktop/Professional course/pandas/ecommerce_sales_analysis.csv')
+print(df_Csv)
+
+print(df_Csv.head())
+print(df_Csv.tail())
+print(df_Csv.describe())
+print(df_Csv.info())
+ 
