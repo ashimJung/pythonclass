5805cafb063aa3f4ce0b2ca31767ec7b75c654d1
ashimJung <ashimkarki420@gmail.com>
2024-07-15 00:10:36 +0545

    revision file

---
+++ revision.py
@@ -0,0 +1,21 @@
+list_number=[12,33,45,78,322,56,77,90,111,112]
+lis=[]
+for num in list_number:
+  if num%2==0:
+    print(num,"is even number")
+    lis.append(num)
+    print(lis)
+
+class class_name:
+  def __init__(self,name):
+    self.name=name
+    print(400+400)
+    print("This will be called whenever class is called or intializer")
+
+
+  def sum_now(self):
+      print("This is sum now")
+      print(400-120)
+
+
+
